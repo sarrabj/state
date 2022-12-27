5655cd5593ac727a89741497626b1b4838ca72fd
sarrabj <sarrabenjdida92@gmail.com>
2022-12-27 18:37:24 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import React, { Component } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+
+
+class App extends React.Component {
+
+
+        state = {
+        Person:{
+        fullName:"Sarra Ben Jdida",
+        bio:"Licence mkg/Master mkg",
+        src: "./images.jpg",
+        profession:"Etudiante"
+      },
+      Show:false,
+      count:0,
+    
+    };
+    
+        
+        handleClick=()=>this.setState({Show:!this.state.Show})
+        
+        componentDidMount(){
+
+
+
+          setInterval(() => {
+            this.setState({count:this.state.count++})
+          },1000);
+        }
+        
+
+  render() {
+    return(
+    
+    <div className='parent'>
+
+  {this.state.Show?  
+  <> 
+    <h5 >Full Name: {this.state.Person.fullName}</h5>
+      <h5 > Bio: {this.state.Person.bio}</h5>
+      <h5 > Profession: {this.state.Person.profession}</h5>
+  <img src={this.state.Person.src} alt="images"/>
+  <p className='count'>Counter: {this.state.count}</p>
+
+  </> :  <></> }
+  
+    <button className='button' onClick={this.handleClick}>Show </button>
+
     </div>
-  );
+    )
+  }
 }
 
+
 export default App;
+
+
+
